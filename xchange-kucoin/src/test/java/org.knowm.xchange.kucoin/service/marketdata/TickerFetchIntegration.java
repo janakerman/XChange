c0407f50b0014fb1332558f837e23f7e05ee0648
Jan Akerman <j.f.akerman@gmail.com>
2018-01-22 21:56:48 +0000

    [kucoin] Adds unit test for all tickers call

--- xchange-kucoin/src/test/java/org.knowm.xchange.kucoin/service/marketdata/TickerFetchIntegration.java
+++ xchange-kucoin/src/test/java/org.knowm.xchange.kucoin/service/marketdata/TickerFetchIntegration.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.Test;
 import org.knowm.xchange.Exchange;
@@ -11,6 +12,7 @@ import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.marketdata.Ticker;
 import org.knowm.xchange.kucoin.KucoinExchange;
+import org.knowm.xchange.kucoin.service.KucoinMarketDataService;
 import org.knowm.xchange.service.marketdata.MarketDataService;
 
 public class TickerFetchIntegration {
@@ -30,4 +32,36 @@ public class TickerFetchIntegration {
     assertThat(ticker.getCurrencyPair()).isEqualTo(CurrencyPair.LTC_BTC);
     assertThat(ticker.getTimestamp()).isNotNull();
   }
+
+  @Test
+  public void fetch_tickers_test() throws IOException {
+    Exchange exchange = ExchangeFactory.INSTANCE.createExchange(KucoinExchange.class);
+    KucoinMarketDataService marketDataService = (KucoinMarketDataService)exchange.getMarketDataService();
+
+    List<Ticker> tickers = marketDataService.getTickers();
+
+    Ticker ticker;
+
+    ticker = tickers.stream().filter(ticker1 -> ticker1.getCurrencyPair().equals(CurrencyPair.BTC_USDT))
+        .findFirst()
+        .get();
+    assertThat(ticker.getBid()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getAsk()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getHigh()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getLast()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getVolume()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getTimestamp()).isNotNull();
+
+    ticker = tickers.stream().filter(ticker1 -> ticker1.getCurrencyPair().equals(CurrencyPair.BCH_BTC))
+        .findFirst()
+        .get();
+    assertThat(ticker.getBid()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getAsk()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getHigh()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getLast()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getVolume()).isGreaterThan(BigDecimal.ZERO);
+    assertThat(ticker.getTimestamp()).isNotNull();
+  }
+
+
 }
